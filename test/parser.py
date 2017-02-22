84196dd06082f68776dd604fe3ea8269476bd2c2
Matt Putnam <matt.s.putnam@gmail.com>
2017-02-22 00:14:29 -0500

    cleanup tests

--- test/parser.py
+++ test/parser.py
@@ -2,133 +2,108 @@ import unittest
 from src.parser import Parser
 
 
-def make_data(main_data=None, show_data=None, local_data=None):
-    return {'main': main_data if main_data else {},
-            'show': show_data if show_data else {},
-            'local': local_data if local_data else {}}
+def test(self, template, expected, data={}, depth=0):
+    parser = Parser(data, depth)
+    rendered = parser.evaluate(template)
+    self.assertEqual(expected, rendered)
 
 
 class ParserTest(unittest.TestCase):
-    def test_make_data(self):
-        main_data = {'foo': 'bar'}
-        local_data = {'a': 'b'}
-        data = make_data(main_data = main_data, local_data = local_data)
-        expected = {'main': main_data, 'show': {}, 'local': local_data}
-        self.assertEqual(expected, data)
-
     def test_no_tokens(self):
-        text = "this is some text"
-        parser = Parser(make_data())
-        rendered = parser.evaluate(text)
-        self.assertEqual(text, rendered)
+        test(self, template='this is some text', expected='this is some text')
 
     def test_variables(self):
-        text = "{{$local.greeting}}, {{$main.name.first}}!"
-        local_data = {'greeting': "Hello"}
-        main_data = {'name': {'first': 'John', 'last': 'Smith'}}
-        parser = Parser(make_data(local_data=local_data, main_data=main_data))
-        rendered = parser.evaluate(text)
-        expected = 'Hello, John!'
-        self.assertEqual(expected, rendered)
+        test(self,
+             template="{{$local.greeting}}, {{$main.name.first}}!",
+             expected='Hello, John!',
+             data={'local': {'greeting': 'Hello'},
+                   'main': {'name': {'first': 'John', 'last': 'Smith'}}})
 
     def test_nested_variables(self):
-        text = "{{$data.($data.keyA.keyB).keyC}}"
-        parser = Parser({'data': {'keyA': {'keyB': 'foo'},
+        test(self,
+             template='{{$data.($data.keyA.keyB).keyC}}',
+             expected='bar',
+             data={'data': {'keyA': {'keyB': 'foo'},
                                   'foo': {'keyC': 'bar'}}})
-        rendered = parser.evaluate(text)
-        expected = 'bar'
-        self.assertEqual(expected, rendered)
 
     def test_foreach(self):
-        text = '{{$main.header}}\n' \
-               '{{foreach:var=$foo, source=$main.data}}[test $foo]\n' \
-               '{{end}}\n' \
-               '{{$main.footer}}'
-        main_data = {'header': 'start', 'data': ['a', 'b', 'c'], 'footer': 'end'}
-        parser = Parser(make_data(main_data=main_data))
-        rendered = parser.evaluate(text)
-        expected = 'start\n' \
-                   '[test a]\n' \
-                   '[test b]\n' \
-                   '[test c]\n' \
-                   '\n' \
-                   'end'
-        self.assertEqual(expected, rendered)
+        test(self,
+             template='{{$main.header}}\n'
+                      '{{foreach:var=$foo, source=$main.data}}[test $foo]\n'
+                      '{{end}}\n'
+                      '{{$main.footer}}',
+             expected='start\n'
+                      '[test a]\n'
+                      '[test b]\n'
+                      '[test c]\n'
+                      '\n'
+                      'end',
+             data={'main': {'header': 'start', 'data': ['a', 'b', 'c'], 'footer': 'end'}})
 
     def test_foreach_cartesian(self):
-        text = '{{foreach:var=$row, source=$data.rows}}' \
-               '{{foreach:var=$col, source=$data.cols}}' \
-               '[$row $col]' \
-               '{{end}}{{end}}'
-        parser = Parser({'data': {'rows': [1, 2, 3], 'cols': ['A', 'B', 'C']}})
-        rendered = parser.evaluate(text)
-        expected = '[1 A][1 B][1 C][2 A][2 B][2 C][3 A][3 B][3 C]'
-        self.assertEqual(expected, rendered)
+        test(self,
+             template='{{foreach:var=$row, source=$data.rows}}'
+                      '{{foreach:var=$col, source=$data.cols}}'
+                      '[$row $col]'
+                      '{{end}}{{end}}',
+             expected='[1 A][1 B][1 C][2 A][2 B][2 C][3 A][3 B][3 C]',
+             data={'data': {'rows': [1, 2, 3], 'cols': ['A', 'B', 'C']}})
 
     def test_resource(self):
-        text = "<img src={{resource:file=test/file.txt}} />"
-        parser = Parser({}, depth=2)
-        rendered = parser.evaluate(text)
-        expected = '<img src=../../test/file.txt />'
-        self.assertEqual(expected, rendered)
+        test(self, depth=2,
+             template='<img src={{resource:file=test/file.txt}} />',
+             expected='<img src=../../test/file.txt />')
 
     def test_resource_var(self):
-        text = "<img src={{resource:file=$test.file}} />"
-        parser = Parser({'test': {'file': 'graphic.png'}}, depth=1)
-        rendered = parser.evaluate(text)
-        expected = '<img src=../graphic.png />'
-        self.assertEqual(expected, rendered)
+        test(self, depth=1,
+             template='<img src={{resource:file=$test.file}} />',
+             expected='<img src=../graphic.png />',
+             data={'test': {'file': 'graphic.png'}})
 
     def test_if_no_var(self):
-        text = "{{if:condition=$foo.dne}}{{$main.true}}\n" \
-               "{{else}}{{$main.false}}\n" \
-               "{{end}}"
-        parser = Parser({'main': {'true': 'Condition passed', 'false': 'expected'}})
-        rendered = parser.evaluate(text)
-        expected = 'expected\n'
-        self.assertEqual(expected, rendered)
+        test(self,
+             template='{{if:condition=$foo.dne}}{{$main.true}}\n'
+                      '{{else}}{{$main.false}}\n'
+                      '{{end}}',
+             expected='expected\n',
+             data={'main': {'true': 'Condition passed', 'false': 'expected'}})
 
-    def test_if_true(self):
-        text = "{{if:condition=$foo.hello}}YES{{end}}"
+    def test_if_true_bool(self):
+        test(self,
+             template='{{if:condition=$foo.hello}}YES{{end}}',
+             expected='YES',
+             data={'foo': {'hello': True}})
 
-        parser = Parser({'foo': {'hello': True}})
-        rendered = parser.evaluate(text)
-        expected = 'YES'
-        self.assertEqual(expected, rendered)
+    def test_if_true_val(self):
+        test(self,
+             template='{{if:condition=$foo.hello}}YES{{end}}',
+             expected='YES',
+             data={'foo': {'hello': 'strings are truthy'}})
 
-        parser = Parser({'foo': {'hello': 'strings are truthy'}})
-        rendered = parser.evaluate(text)
-        expected = 'YES'
-        self.assertEqual(expected, rendered)
+    def test_if_false_no_else_boolean(self):
+        test(self,
+             template='{{if:condition=$foo.hello}}YES{{end}}',
+             expected='',
+             data={'foo': {'hello': False}})
 
-    def test_if_false_no_else(self):
-        text = "{{if:condition=$foo.hello}}YES{{end}}"
-
-        parser = Parser({'foo': {'hello': False}})
-        rendered = parser.evaluate(text)
-        expected = ''
-        self.assertEqual(expected, rendered)
-
-        parser = Parser({'foo': {}}) # None is falsy
-        rendered = parser.evaluate(text)
-        expected = ''
-        self.assertEqual(expected, rendered)
+    def test_if_false_no_else_none(self):
+        test(self,
+             template='{{if:condition=$foo.hello}}YES{{end}}',
+             expected='',
+             data={'foo': {'hello': None}})
 
     def test_eval_no_vars(self):
-        text = 'abc {{eval(2 + 2)}} xyz'
-        parser = Parser({})
-        rendered = parser.evaluate(text)
-        expected = 'abc 4 xyz'
-        self.assertEqual(expected, rendered)
+        test(self,
+             template='abc {{eval(2 + 2)}} xyz',
+             expected='abc 4 xyz')
 
     def test_eval_vars(self):
-        text = '{{$head}} {{eval($nums.x + $nums.($data.var))}} {{$tail}}'
-        parser = Parser({'head': 'abc', 'tail': 'xyz',
-                         'data': {'var': 'y'},
-                         'nums': {'x': '42', 'y': 11}})
-        rendered = parser.evaluate(text)
-        expected = 'abc 53 xyz'
-        self.assertEqual(expected, rendered)
+        test(self,
+             template='{{$head}} {{eval($nums.x + $nums.($data.var))}} {{$tail}}',
+             expected='abc 53 xyz',
+             data={'head': 'abc', 'tail': 'xyz',
+                   'data': {'var': 'y'},
+                   'nums': {'x': '42', 'y': 11}})
 
 
 if __name__ == '__main__':
