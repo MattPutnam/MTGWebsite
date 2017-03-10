bc9d7702efe0be896d1646031ef94ecba08696d7
Matt Putnam <matt.s.putnam@gmail.com>
2017-03-09 23:02:00 -0500

    get everything all split out again

--- src/parser/__init__.py
+++ src/parser/__init__.py
@@ -1,1 +1,2 @@
-from parser.parser import compile_template, evaluate_template, TemplateData
+from parser.parser import compile_template, evaluate_template
+from parser.template_data import TemplateData
